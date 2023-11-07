e980bbc4a70acc790e272a5a769fc9b461e51cda
Luis Osuna <luisosuna@Luiss-MacBook-Pro.local>
2023-11-07 08:57:00 -0700

    Project initial files

---
+++ src/test/java/com/luis/jenkins/JenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luis.jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
